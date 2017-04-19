f5c31b618f7751fe34a01b621a4c8d6bd495ab62
glc3344 <glc3344@gmail.com>
2017-04-19 12:35:01 -0400

    feat: created models for grocery categories
    created the database structure for all the various grocery categories ie (dairy, produce, meat, etc)

--- groceries/models.py
+++ groceries/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=32)
+    quantity = models.IntegerField()
+    weight = models.IntegerField()
+
+
+class Produce(Item):
+    pass
+
+
+class Meat_Seafood(Item):
+    pass
+
+
+class Dairy(Item):
+    pass
+
+
+class Frozen_Food(Item):
+    pass
+
+
+class Breads_Pasta(Item):
+    pass
+
+
+class Canned_Goods(Item):
+    pass
+
+
+class Snacks(Item):
+    pass
+
+
+class Baking_Condiments(Item):
+    pass
+
+
+class Paper_Plastic(Item):
+    pass
+
+
+class Cleaning_Products(Item):
+    pass
+
+
+class Toiletries(Item):
+    pass
+
+
+class Misc(Item):
+    pass
